e57d1e4f7c3de86ff00090b229954d0728a06bfd
Arci <Commanderarci@hotmail.Be>
2020-02-11 15:32:03 +0100

    More updates that add stuff, manly the choice between editor and gameplay, editor will be a map maker, item maker, entity maker.. and gameplay will be gameplay.

---
+++ MenuVorm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace TextVenture
+{
+    class MenuVorm
+    {
+        string[] MenuOptions;
+        string MainMenuTittle;
+        bool BEGONE;
+
+        public MenuVorm(string MainMenu, string[] InputOptions, bool IBEGONE = true)
+        {
+            MainMenuTittle = MainMenu;
+            MenuOptions = InputOptions;
+            BEGONE = IBEGONE;
+        }
+
+        public string Show()
+        {
+            int Ow = Console.CursorLeft;
+            int Oh = Console.CursorTop;
+            ConsoleKeyInfo cki;
+            int sel = 0, AnimationFrame = 0;
+            bool EnterPressed = false;
+            do
+            {
+                //Clear Selections
+                for (int P = 0; P > MainMenuTittle.Length; P++)
+                {
+                    Console.Write(" ");
+                }
+                for (int o = 0; o < MenuOptions.Length; o++)
+                {
+                    Console.Write(" ");
+                    Console.WriteLine();
+                    for (int P = 0; P > MenuOptions[o].Length; P++)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.SetCursorPosition(Ow, Oh);
+
+
+                Console.WriteLine(" --- " + MainMenuTittle + " --- ");
+
+
+                for (int i = 0; i < MenuOptions.Length; i++)
+                {
+                    if (sel > MenuOptions.Length || sel < 0)
+                    {
+                        sel = 0;
+                    }
+                    if (sel == i)
+                    {
+                        if (AnimationFrame > 5)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                        }
+
+                    }
+                    Console.Write("-");
+                    Console.WriteLine(MenuOptions[i]);
+                    Console.ResetColor();
+                }
+                AnimationFrame++;
+                if (AnimationFrame > 10)
+                {
+                    AnimationFrame = 0;
+                }
+                System.Threading.Thread.Sleep(100); // This is 1/10 of a second Basicly there are 10 frames each second
+
+                if (Console.KeyAvailable)
+                {
+                    cki = Console.ReadKey();
+                    switch (cki.Key)
+                    {
+                        case (ConsoleKey.UpArrow):
+                            sel--;
+                            break;
+                        case (ConsoleKey.DownArrow):
+                            sel++;
+                            break;
+                        case (ConsoleKey.Enter):
+                            EnterPressed = true;
+                            break;
+                        default:
+                            break;
+                    }
+                    if (sel > MenuOptions.Length - 1)
+                        sel = MenuOptions.Length - 1;
+                    if (sel < 0)
+                        sel = 0;
+                }
+
+            } while (EnterPressed == false);
+
+            //If you want the Menu gone after its finished its job
+
+            if (BEGONE == true)
+            {
+                //Untill SOMEBODY FIXED THE DELETION CODE IMA JUST PUT THIS HERE
+                Console.SetCursorPosition(Ow, Oh);
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(" --- " + MainMenuTittle + " --- ");
+                for (int i = 0; i < MenuOptions.Length; i++)
+                {
+                    Console.Write("-");
+                    Console.WriteLine(MenuOptions[i]);
+                }
+                Console.SetCursorPosition(Ow, Oh);
+            }
+            Console.ResetColor();
+            return MenuOptions[sel];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 
 namespace TextVenture
 {
@@ -10,12 +9,47 @@ namespace TextVenture
             NormalText defaultText = new NormalText(50);
             defaultText.Text("Hi there young one, how are you doing?");
 
-            defaultText.Text("Now with colors!",ConsoleColor.Red);
+            defaultText.Text("Now with colors!", ConsoleColor.Red);
             defaultText.Text("And even more colors", ConsoleColor.Blue);
+
+            MenuVorm DebugStart = new MenuVorm("Debug mode", new string[] { "Start Game", "Editor" }, true);
+
+
+
+
+            switch (DebugStart.Show())
+            {
+                case "Start Game":
+                    Loader();
+                    GameLoop();
+                    break;
+                case "Editor":
+                    Editor();
+                    break;
+            }
+
 
 
             Console.ReadKey();
+
+
+            void Loader()
+            {
+                //This will read all the gamedata from a direcotory
+
+            }
+
+            void GameLoop()
+            {
+
+            }
+            void Editor()
+            {
+
+            }
         }
+
+
 
 
     }
