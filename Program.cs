a5f0c3451868bb147a2679aeef1fc9786ac23f6d
Arci <Commanderarci@hotmail.Be>
2020-02-11 14:01:09 +0100

    added examples for dumbasses

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace TextVenture
             NormalText defaultText = new NormalText(50);
             defaultText.Text("Hi there young one, how are you doing?");
 
-
+            defaultText.Text("Now with colors!",ConsoleColor.Red);
+            defaultText.Text("And even more colors", ConsoleColor.Blue);
 
 
             Console.ReadKey();
